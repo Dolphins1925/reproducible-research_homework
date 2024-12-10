b6381b0aa23ba67b9fe451b20709692237d18a1f
Julia Morgan <julia.morgan@stcatz.ox.ac.uk>
2024-12-10 18:09:27 +0000

    Assingment edits

--- question-4-code/random_walk.R
+++ question-4-code/random_walk.R
@@ -4,23 +4,27 @@
 library(ggplot2)
 library(gridExtra)
 
-random_walk  <- function (n_steps) {
+random_walk  <- function (n_steps, seed = NULL) {
+  
+  if (!is.null(seed)) {
+    set.seed(seed) # set the seed for reproducibility
+  }
   
   df <- data.frame(x = rep(NA, n_steps), y = rep(NA, n_steps), time = 1:n_steps)
   
-  df[1,] <- c(0,0,1)
+  df[1,] <- c(0,0,1) # starting point
   
   for (i in 2:n_steps) {
     
-    h <- 0.25
+    h <- 0.25 # step size
     
-    angle <- runif(1, min = 0, max = 2*pi)
+    angle <- runif(1, min = 0, max = 2*pi) # random angle 
     
-    df[i,1] <- df[i-1,1] + cos(angle)*h
+    df[i,1] <- df[i-1,1] + cos(angle)*h # update x-coordination
     
-    df[i,2] <- df[i-1,2] + sin(angle)*h
+    df[i,2] <- df[i-1,2] + sin(angle)*h # update y-coordinate
     
-    df[i,3] <- i
+    df[i,3] <- i # update time step
     
   }
   
@@ -28,7 +32,9 @@ random_walk  <- function (n_steps) {
   
 }
 
-data1 <- random_walk(500)
+# simulate two random walks with reproducible seeds
+
+data1 <- random_walk(500, seed = 25 )
 
 plot1 <- ggplot(aes(x = x, y = y), data = data1) +
   
@@ -38,9 +44,12 @@ plot1 <- ggplot(aes(x = x, y = y), data = data1) +
   
   xlab("x-coordinate") +
   
-  ylab("y-coordinate")
+  ylab("y-coordinate") +
+  
+  ggtitle ("Brownian Motion (Seed = 25)")
 
-data2 <- random_walk(500)
+
+data2 <- random_walk(500, seed = 98)
 
 plot2 <- ggplot(aes(x = x, y = y), data = data2) +
   
@@ -50,6 +59,8 @@ plot2 <- ggplot(aes(x = x, y = y), data = data2) +
   
   xlab("x-coordinate") +
   
-  ylab("y-coordinate")
+  ylab("y-coordinate") +
+  
+  ggtitle("Brownian Motion (Seed = 98)")
 
 grid.arrange(plot1, plot2, ncol=2)
