66587438a9c133a8c7441f4697cc83342f2396f7
Julia Morgan <julia.morgan@stcatz.ox.ac.uk>
2024-12-11 14:34:16 +0000

    Final edits to code for question 5

--- question-5-code.R
+++ question-5-code.R
@@ -1,28 +1,32 @@
+#QUESTION 5d CODE ON LINES 70-83
+
+# Load relevant packages and install through the packages tab if needed
 library(dplyr)
 library(ggplot2)
 
+# Check that the data has been loaded in correctly
 head(Cui_etal2014)
 
-# Check the dimensions of the dataset
-rows <- nrow(Cui_etal2014)   # Number of rows
-columns <- ncol(Cui_etal2014) # Number of columns
+# Check the number of rows and columns of the dataset
+nrow(Cui_etal2014)   # 33 rows
+ncol(Cui_etal2014) # 13 columns
+# If code is ran a second time, the number of columns increases from 13 to 15
+# This is due to the added log-transformed columns
 
-rows
-columns
-
+# Check the names of the columns
+# Are the variables I need to use in a good form for R?
 colnames(Cui_etal2014)
 
-
-library(dplyr)
-
+# Make the names of Virion Volume and Genome Size cleaner
 # Rename both columns in one step
-#Cui_etal2014 <- Cui_etal2014 %>%
+Cui_etal2014 <- Cui_etal2014 %>%
   rename(
     virion_volume = `Virion volume (nmxnmxnm)`,
     genome_size = `Genome length (kb)`
   )
+#  The dataset could be renamed so that the original is not overwritten
 
-
+# Check that R recognises these new names
 Cui_etal2014$genome_size
 Cui_etal2014$virion_volume
 
@@ -31,17 +35,19 @@ plot(Cui_etal2014$genome_size, Cui_etal2014$virion_volume,
      main = "Scatter Plot of Virion Volume vs Genome Size",
      xlab = "Genome Size", ylab = "Virion Volume", pch = 19)
 
-# Log transformation for volume if necessary
-#Cui_etal2014$log_volume <- log(Cui_etal2014$virion_volume)
-#Cui_etal2014$log_genome_size <- log(Cui_etal2014$genome_size)
+# Log transformation for volume and genome size to fit a linear model
+Cui_etal2014$log_volume <- log(Cui_etal2014$virion_volume)
+Cui_etal2014$log_genome_size <- log(Cui_etal2014$genome_size)
 
 # Fit a linear model: Log-transformed volume predicted by genome size
 model <- lm(log_volume ~ log_genome_size, data = Cui_etal2014)
 
 # Summarize the model
 summary(model)
+# Intercept 7.0748, p-value is significant
+# Log genome size 1.5152, p-value is significant
 
-# Visualize the fitted model
+# Visualise the fitted model
 plot(Cui_etal2014$log_genome_size, Cui_etal2014$log_volume, 
      main = "Log-Transformed Volume vs Log-Transformed Genome Size",
      xlab = "log(Genome Size)", ylab = "log(Volume)", pch = 19)
@@ -50,6 +56,7 @@ abline(model, col = "red")
 # Check residuals for diagnostics
 par(mfrow = c(2, 2))
 plot(model)
+# The linear model is not perfect 
 
 alpha <- exp(coef(model)[1])  # Scaling factor α
 beta <- coef(model)[2]       # Exponent β
@@ -60,25 +67,29 @@ beta
 p_values <- summary(model)$coefficients[, 4]
 p_values
 
+###################
 
+# QUESTION 5d)
 # Create the scatter plot with the regression line
 ggplot(Cui_etal2014, aes(x = log_genome_size, y = log_volume)) +
   geom_point() +  # scatter plot
   geom_smooth(method = "lm", color = "blue", fill = "gray") +  # linear regression with confidence interval
   labs(
     x = "log [Genome length (kb)]",
-    y = "log [Virion volume (mm³)]"
+    y = "log [Virion volume (nm³)]"
   ) +
-  theme_minimal()  # optional: for cleaner background
+  theme_minimal() # set theme the same as image
 
-# Define parameters
+####################
+
+# Define parameters for estimayted volume of a 300kb dsDNA virus
 alpha <- 1181.807
 beta <- 1.515228
 L <- 300  # Genome length in kb
 
 # Estimate virion volume (V)
-V_estimate <- alpha * (L ^ beta)
+Volestimate <- alpha * (L ^ beta)
 
 # Print the estimated volume
-print(V_estimate)
+print(Volestimate)
 
